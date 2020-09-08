d9eadc0caae305cd6035b25e5a1c39e037b54234
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-09-08 12:11:24 -0500

    feat(footer) Add footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
