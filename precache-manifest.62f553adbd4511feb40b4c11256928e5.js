b2ecb933f26b34a92a84014d7a92177ed054cf07
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-09-08 14:54:39 -0500

    Updates

---
+++ precache-manifest.62f553adbd4511feb40b4c11256928e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55ef27197123526a157bcd91f8d8e19",
+    "url": "/react-airbnb-clone/index.html"
+  },
+  {
+    "revision": "2fd7db7de9a3317cf5f3",
+    "url": "/react-airbnb-clone/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "c294d9ad0bcc1e3f9e95",
+    "url": "/react-airbnb-clone/static/css/main.d14c0e74.chunk.css"
+  },
+  {
+    "revision": "2fd7db7de9a3317cf5f3",
+    "url": "/react-airbnb-clone/static/js/2.ad230222.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-airbnb-clone/static/js/2.ad230222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c294d9ad0bcc1e3f9e95",
+    "url": "/react-airbnb-clone/static/js/main.7452ae89.chunk.js"
+  },
+  {
+    "revision": "3a76ea3c5f15e9e3dbcc",
+    "url": "/react-airbnb-clone/static/js/runtime-main.8dbe4b36.js"
+  }
+]);
